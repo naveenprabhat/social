69b66a42e397b66fb47192120a49fadef8a03dcf
naveen.bhu <prabhatnaveen20@gmail.com>
2023-01-13 14:21:28 +0530

    Added https://test-api.skalebot.com/ domain

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const passport = require("passport");
 const authRoute = require("./routes/auth");
 const app = express();
 
-const CLIENT_URL = "http://localhost:3000";
+const CLIENT_URL = "https://test-api.skalebot.com";
 
 app.use(
   cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
